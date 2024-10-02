7976e63838e4192c66556ac2cfaa38f4731ecfd1
amargiela <austinmargiela@gmail.com>
2024-10-02 11:08:52 -0400

    Update main.java

--- main.java
+++ main.java
@@ -1,3 +1,16 @@
-public class main {
-  random
-}
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class ArticleProcessor {
+
+    private List<String> stopWords;
+
+    public ArticleProcessor(String stopWordsFilePath) throws FileNotFoundException {
+        stopWords = new ArrayList<>();
+        Scanner sc = new Scanner(new File(stopWordsFilePath));
+        while (sc.hasNext()) {
+            stopWords.add(sc.next());
+        }
+        sc.close();
+    }
